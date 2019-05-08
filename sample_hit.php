03b3719bafb293fd6dedfb48345fc5a8a47b947e
Muhammad Khoirul Umam <www.umam@gmail.com>
2019-05-08 10:32:51 +0700

    Update due to server migrations

--- sample_hit.php
+++ sample_hit.php
@@ -80,11 +80,10 @@ $interface = new ApiInterface();
 $params['trx_date'] = date("YmdHis");
 $params['trx_id'] = generateTrxId(10);
 $params['trx_type'] = '2100'; // 2100 = Inquiry, 2200 = Payment
-$params['cust_msisdn'] = '01428800711';
-$params['cust_account_no'] = '01428800711';
-$params['product_id'] = '80'; // 80 = PLN Prepaid
-$params['product_nomination'] = ''; 
-// $params['product_nomination'] = '20000'; // use this line to makes payment request
+$params['cust_msisdn'] = '';
+$params['cust_account_no'] = '212100089886';
+$params['product_id'] = '100';
+$params['product_nomination'] = '';
 $params['periode_payment'] = '';
 $params['unsold'] = '';
 $input = json_encode($params, true);
@@ -134,7 +133,6 @@ $rc = $data['rc'];
 if ($rc == '0000') {
 	echo "<pre>";
 	echo "Transaction ID -> " . $data['trx_id'] . "<br />";
-	echo "Material Number -> " . $data['material_number'] . "<br />";
 	echo "Subscriber ID -> " . $data['subscriber_id'] . "<br />";
 	echo "Subscriber Name -> " . $data['subscriber_name'];
 	echo "</pre>";
